00f5cb60634e395db8061d995cebb746b578a4e1
Alejandro Brugarolas <alejandro_brugarolas@hotmail.com>
2022-03-31 23:52:41 +0200

    add songs and meta tags

--- src/app/core/constants/songs.conts.ts
+++ src/app/core/constants/songs.conts.ts
@@ -6,6 +6,7 @@ export const SONG_NAMES: string[] = [
   'Yonaguni - Bad Bunny',
   'Cuando zarpa el amor - Camela',
   'Besos En Guerra - Morat',
+  'Cómo Te Atreves - Morat',
   'Purpurina - Alberto gambino',
   'Yo Te Esperaré - Cali Y El Dandee',
   'Un Beso y una Flor - Nino Bravo',
@@ -13,6 +14,8 @@ export const SONG_NAMES: string[] = [
   'Tu jardín con enanitos - Melendi',
   '23 de Junio - Vetusta Morla',
   'Marta, Sebas, Guille y los demás - Amaral',
+  'Sin ti no soy nada - Amaral',
+  'El universo sobre mí - Amaral',
   'Zapatillas - El Canto Del Loco',
   'Pan y mantequilla - Efecto Pasillo',
   'Te Pintaron Pajaritos - Yandar & Yostin',
@@ -37,49 +40,70 @@ export const SONG_NAMES: string[] = [
   'Noches de Boda - Joaquín Sabina',
   'Soy un Truhán, Soy un Señor - Julio Iglesias',
   'Pacto Entre Caballeros - Joaquín Sabina',
-
-  
+  'Si te vas... - Extremoduro',
+  'La vereda de la puerta de atrás - Extremoduro',
+  'Stand By - Extremoduro',
+  'Molinos De Viento - Mägo de Oz',
+  'Haste Que El Cuerpo Aguante - Mägo de Oz',
+  'La danza del fuego - Mägo de Oz',
+  'Si Amaneciera - Saratoga',
+  'Nunca debí enamorarme - Camela',
+  'Noche De Halloween - Saurom',
+  'El Hada & La Luna - Saurom',
+  'Soñando Contigo (Las Caricias Del Alma) - Saurom',
 ];
 
 export const SONG_IDS: string[] = [
   '0mGvaKpwLYoybnIoboC3uS',
   '3Th2of8xIYyImVLXUbzaTp',
-  '2f0Ft2XdvNSMSY5TUlZgEz',
-  //'1mlGScrDQqHqmhdIqE8MmA',
+  '2f0Ft2XdvNSMSY5TUlZgEz', // s
   '4JOsL8WntMIwDeNQlKAwdA',
   '0GzQB5WXavUEU8tfTCAd8m',
+  '1e52ifcQRamZqYaHCOmAdp', // Soñando Contigo (Las Caricias Del Alma) - Saurom
+  //'5IU4JO2cpa0wPK8oQWFfCr',
+  //'1mlGScrDQqHqmhdIqE8MmA',
   '2JPLbjOn0wPCngEot2STUS',
   '6HB8EOChKaIJSoSZbpJacU',
   '6yCjU5j6YHbux2VQCBTTE1',
+  '5Q2uU5NGcS0mKwmgF28kRZ', // Haste Que El Cuerpo Aguante - Mägo de Oz
   '3NolL8Ds5qRrYQSVUBKicn',
-  '30ddpolNHwXlrS5XO8MiiO',
   '42VgiRyg0YjtRVLiPFNxPJ',
-  '2kZJ5wrpq1gCdHmVEjahoH',
+  '3bhgoiydVfrHlWNsXI9BE4', // Si Amaneciera - Saratoga
   '419w8oZaqevgyZfdvFZEHa',
   '1VB4sadHjFcFklHcZuoROi',
   '0DXI6c9tNWsI4tvUftkQN8',
   '0eVborSuxUeSg0meWYd9dZ', // s
   '4BtggpHp17Ak3RP0LITrue', // s
   '5HFvuMLMgS75OaY3UKzqqs', // s
-  //'5IU4JO2cpa0wPK8oQWFfCr',
   '5OaqffbeQyVVqtnA71Ci7K',
+  '0dQPkfweyWDzZ20Auq3F14', // La danza del fuego - Mägo de Oz
   '2LsbizbOeNa4x6qsi2jAMb',
-  '7gD2QkknRa4ZvlRVyU6ZO0',
   '3hlWUNjAlbZ6nOjAbrivBv',
+  '70f6HTczJo4mjv4YzVGkl8', // Nunca debí enamorarme - Camela
   '78GUxsLIyH3ZohwhNCTKES',
   '5HSpMRUC4m3L3q48to2Kmu',
   '0wQCKR9OFjYu5Kzrk7WivJ',
+  '6BxCmy6vGbuOckxg6YfQOW', // Stand By - Extremoduro
   '1aZc9YBrZpKCE1YqSMV5Vg',
   '41JrRedwrJxbBPwI8Qku04',
   '020Gj87SqDRvzGHELzcCEc',
-  '7FXZbH91wnIsipIl6LDwkU',
+  '1PeymV5oAghrPOA4vWdYf5', // El Hada & La Luna - Saurom
   '13nyc1sxYqFDmaGyr1bmYP',
   '0ikKymkbnJhV0S01mfH2El',
-  '7Bewui7KtaMzROeteRitRz',
+  '1jJn6iK6o2Vg7iUKHztRTk', // El universo sobre mí - Amaral
   '4Kii4OhkRIezu0KjOiFq0G',
   '1eCie6lwjb4crxeAvkRaSi',
+  '19vhfSUgVJO2enJ6XidUGO', // Molinos De Viento - Mägo de Oz
   '0FfxAxOlgIUaemwaesR0V4',
+  '2kZJ5wrpq1gCdHmVEjahoH',
+  '2x59pa3xDf8hHZ4N3f5fGQ', // Si te vas... - Extremoduro
+  '30ddpolNHwXlrS5XO8MiiO',
+  '4kJP8Z888wREJ8bRMWNMuk', // La vereda de la puerta de atrás - Extremoduro
+  '7Bewui7KtaMzROeteRitRz',
+  '7cQ7axcCXsa963OluHTWlS', // Sin ti no soy nada - Amaral
+  '7FXZbH91wnIsipIl6LDwkU',
+  '7M6CFruBrM5x7u0lTMtm6r', // Cómo Te Atreves - Morat
   '3R8rzNolKgeMo3MQhYFBgu',
-
-
+  '7LOB6BlP57YO1ezKxz5py4', // Noche De Halloween - Saurom
+  '7gD2QkknRa4ZvlRVyU6ZO0',
 ];
